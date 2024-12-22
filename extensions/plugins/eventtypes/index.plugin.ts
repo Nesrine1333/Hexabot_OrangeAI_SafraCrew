cc70043c304def4331b2368e35441c646d660eae
Nourjeday <141372467+Nourjeday@users.noreply.github.com>
2024-12-22 03:03:35 +0100

    fetch types implemented

---
+++ extensions/plugins/eventtypes/index.plugin.ts
@@ -0,0 +1,123 @@
+import { Block } from '@/chat/schemas/block.schema';
+import { Context } from '@/chat/schemas/types/context';
+import {
+  OutgoingMessageFormat,
+  StdOutgoingEnvelope,
+  StdOutgoingQuickRepliesEnvelope,
+} from '@/chat/schemas/types/message';
+import {
+  QuickReplyType,
+  StdQuickReply,
+} from '@/chat/schemas/types/quick-reply';
+import { BlockService } from '@/chat/services/block.service';
+import { SubscriberService } from '@/chat/services/subscriber.service';
+import { BaseBlockPlugin } from '@/plugins/base-block-plugin';
+import { PluginService } from '@/plugins/plugins.service';
+import { PluginBlockTemplate } from '@/plugins/types';
+import { SettingService } from '@/setting/services/setting.service';
+import { Injectable } from '@nestjs/common';
+import SETTINGS from './settings';
+
+@Injectable()
+export class JustPlugin extends BaseBlockPlugin<typeof SETTINGS> {
+  template: PluginBlockTemplate = {
+    patterns: ['time'],
+    starts_conversation: true,
+    name: 'Get Event Types',
+  };
+
+  constructor(
+    pluginService: PluginService,
+    private readonly blockService: BlockService,
+    private readonly settingService: SettingService,
+    private readonly subscriberService: SubscriberService,
+  ) {
+    super('event-types-plugin', pluginService);
+  }
+
+  getPath(): string {
+    return __dirname;
+  }
+
+  async process(
+    block: Block,
+    context: Context,
+    _convId: string,
+  ): Promise<StdOutgoingEnvelope> {
+    const args = this.getArguments(block);
+    const settings = await this.settingService.getSettings();
+
+    const params = new URLSearchParams({
+      user: args.userUri,
+    });
+    console.log('vars' + context.vars.location);
+
+    const eventTypes = await fetch(
+      `https://api.calendly.com/event_types?${params.toString()}`,
+      {
+        headers: {
+          Authorization: 'Bearer ' + args.CalendlyToken,
+          'Content-Type': 'application/json',
+        },
+      },
+    );
+    const response = await eventTypes.json();
+    // const resp = JSON.stringify(response);
+    const quickReplies = response.collection.map(
+      (event): StdQuickReply => ({
+        content_type: QuickReplyType.text,
+        title: event.name,
+        payload: event.name,
+      }),
+    );
+    const typesArray = await response.collection.map(
+      (event): EventType => ({
+        uri: event.uri,
+        name: event.name,
+      }),
+    );
+    context.vars.typesarray = JSON.stringify(typesArray);
+    console.log(
+      'types arrayhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh' +
+        context.vars.typesarray,
+    );
+    console.log('user id' + context.user.id);
+    await this.subscriberService.updateOne(context.user.id, {
+      context: context,
+    });
+
+    // const blockMessage: BlockMessage =
+    //   false && block.options.fallback
+    //     ? [...block.options.fallback.message]
+    //     : Array.isArray(block.message)
+    //       ? [...block.message]
+    //       : { ...block.message };
+
+    const msg: StdOutgoingQuickRepliesEnvelope = {
+      format: OutgoingMessageFormat.quickReplies,
+      message: {
+        text: this.blockService.processText('zertyuio', context, {}, settings),
+        quickReplies: quickReplies.map((qr: StdQuickReply) => {
+          return qr.title
+            ? {
+                ...qr,
+                title: this.blockService.processText(
+                  qr.title,
+                  context,
+                  {},
+                  settings,
+                ),
+              }
+            : qr;
+        }),
+      },
+    };
+
+    return msg;
+  }
+}
+
+interface EventType {
+  name: string;
+  uri: string;
+}
